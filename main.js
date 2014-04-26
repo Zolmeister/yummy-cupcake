cabb952557ed7afb9a588371ec0e075e579de175
Austin Hallock <austinhallock@gmail.com>
2014-04-26 00:48:17 -0700

    Dynamic svg loading

--- main.js
+++ main.js
@@ -1,31 +1,3 @@
-/*
- * var xhr = new XMLHttpRequest();
-xhr.onload = function() {
-    // get the XML tree of the SVG
-    var svgAsXml = xhr.responseXML;
-    // do some modifications to the XML tree
-    var element = svgAsXml.getElementById('Ribbon');
-    element.style.display = 'none';
-    // convert the XML tree to a string
-    var svgAsString = new XMLSerializer().serializeToString(svgAsXml);
-    // create a new image with the svg string as an ObjectUrl
-    var svgBlob = new Blob([svgAsString], {type: "image/svg+xml;charset=utf-8"});
-    var url = window.URL.createObjectURL(svgBlob);
-    var img = new Image();
-    img.src = url;
-    // copy it to the canvas
-    img.onload = function() {
-        var theCanvas = document.getElementById('canvas');
-        var context = theCanvas.getContext('2d');
-        context.drawImage(img, 0, 0);
-        window.URL.revokeObjectURL(svgBlob);
-    }
-}
-xhr.open("GET", "/images/pink-new.svg");
-xhr.responseType = "document";
-xhr.send();
- */
-
 // not that function calls within update are not optimized,
 // and should be in-lined during some compile step
 var debug = true
@@ -42,7 +14,7 @@ var main_state = {
 
       // Load a sprite in the game
       // Parameters: name of the sprite, path to the image
-      game.load.image('cupcake', 'assets/cupcake.png')
+      game.load.image('cupcake', this.url)
       game.load.image('button', 'assets/button.png')
 
       game.stage.backgroundColor = '#71c5cf'
@@ -58,10 +30,10 @@ var main_state = {
       if (debug)
         this.stage.disableVisibilityChange = true;
 
-      var scoreText = "0 Cupcakes"
-      var scoreTextStyle = { font: "45px Arial", align: "center" }
+      var scoreText = '0 Cupcakes'
+      var scoreTextStyle = { font: '45px Arial', align: 'center' }
       var cpsText = '0 per second'
-      var cpsStyle =  { font: "25px Arial", align: "center" }
+      var cpsStyle =  { font: '25px Arial', align: 'center' }
 
       game.cupcakeCount = 0
       game.scoreText = game.add.text(game.world.centerX-300, 10, scoreText, scoreTextStyle)
@@ -115,7 +87,7 @@ function cupcakeClick(button, pointer) {
   // spawn +1 near the mouse
   //pointer.x
 
-  var plusOneStyle =  { font: "25px Arial", align: "center" }
+  var plusOneStyle =  { font: '25px Arial', align: 'center' }
   var plusOne = game.add.text(pointer.x, pointer.y, '+1', plusOneStyle)
   game.add.tween(plusOne).to({ y: -50 }, Math.random() * 1500 + 2000, Phaser.Easing.Cubic.Out, true);
 
@@ -126,6 +98,55 @@ function loop() {
 
 }
 
+// TODO: probably should have some sort of class/obj we can attach things to
+// all possible items. guessing you're going to re-implement this somewhere
+var items = ['cherry', 'ribbon', 'straw', 'sprinkles']
+function grabCupcakeSVG(options, callback) {
+	var width = options.width
+	var height = options.height
+	// Necessary to grab the XML of the svg
+	var xhr = new XMLHttpRequest()
+	xhr.onload = function() {
+		// parse as XML instead of a string
+		var svgAsXml = xhr.responseXML
+		// find the elements we want to remove
+		var shownItems = options.items
+		for (var i = 0, j = items.length; i < j; i++) {
+			var item = items[i]
+			if(shownItems.indexOf(item) !== -1)
+				continue // skip hiding any item we specified
+			var $toHide = svgAsXml.getElementById(item)
+			$toHide.style.display = 'none'
+		}
+		// change xml back to string
+		var svgAsString = new XMLSerializer().serializeToString(svgAsXml)
+		var svgBlob = new Blob([svgAsString], {type: 'image/svg+xml;charset=utf-8'})
+		// since phaser doesn't support svg, we have to convert to a png of the right size
+		var canvas = document.createElement('canvas')
+		canvas.width = width * window.devicePixelRatio
+		canvas.height = height * window.devicePixelRatio
+		canvas.style.width = width + 'px'
+		canvas.style.height = height + 'px'
+		var ctx = canvas.getContext('2d')
+		var img = new Image()
+		// set the img src to the xml string
+		img.src = window.URL.createObjectURL(svgBlob)
+		img.onload = function() {
+			// draw svg to canvas
+			ctx.drawImage(img, 0, 0, width, height)
+			// canvas to png
+			callback(canvas.toDataURL('images/png'))			
+		}
+	}
+	xhr.open('GET', '/assets/pink.svg')
+	xhr.responseType = 'document'
+	xhr.send()
+}
+
 // And finally we tell Phaser to add and start our 'main' state
 game.state.add('main', main_state)
-game.state.start('main')
+
+grabCupcakeSVG({ width: 326, height: 463, items: ['cherry', 'straw'] }, function(url) {
+	main_state.url = url
+	game.state.start('main')
+})
