b27aa1580dd68c733bb85ed3da1790bbd1e42f88
Zolmeister <zolikahan@gmail.com>
2014-05-14 09:21:13 -0700

    cleaned up Gulpfile

--- Gulpfile.js
+++ Gulpfile.js
@@ -15,82 +15,30 @@ var s3 = require('gulp-s3')
 var revall = require('gulp-rev-all')
 var gzip = require('gulp-gzip')
 var cloudfront = require('gulp-cloudfront')
-var sensitive = require('./sensitive.js') // passwords, keys, etc... use sensitive.js.template as base
+
+// passwords, keys, etc... use sensitive.js.template as base
+var sensitive = require('./sensitive.js')
 var runSequence = require('run-sequence')
 
-// TODO: cleanup vars
+// TODO: Separate out into mulitple files
+var outFiles = {
+  scripts: 'bundle.js',
+  styles: 'bundle.css',
+  vendors: 'vendor.js'
+}
+
 var paths = {
   scripts: ['./app/init.js', './app/js/**/*.js', './app/lib/**/*.js'],
+  init: './app/init.js',
   vendor: './app/vendor/*.js',
   index: './app/index.html',
   styles: './app/css/style.css',
   dist: './app/dist/',
   build: './build/',
-  appcache: './cache.appcache'
+  appcache: './cache.appcache',
+  buildDist: './build/dist/',
+  assets: './app/assets/**/*.*'
 }
-
-gulp.task('scripts', function () {
-  return browserify('./app/init.js')
-    .bundle({debug:true})
-    .pipe(source('bundle.js'))
-    .pipe(gulp.dest(paths.dist))
-})
-
-gulp.task('lint:scripts', function () {
-  return gulp.src(paths.scripts)
-    .pipe(jshint())
-    .pipe(jshint.reporter('default'))
-})
-
-gulp.task('vendor', function () {
-  return gulp.src(paths.vendor)
-    .pipe(sourcemaps.init())
-    .pipe(concat('vendor.js'))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(paths.dist))
-})
-
-gulp.task('styles', function () {
-  return gulp.src(paths.styles)
-    .pipe(rename('bundle.css'))
-    .pipe(gulp.dest(paths.dist))
-})
-
-gulp.task('appcache', function () {
-  return gulp.src(paths.appcache)
-    .pipe(footer('\n#' + Date.now()))
-    .pipe(gulp.dest(paths.build))
-})
-
-gulp.task('copy:index', function () {
-  return gulp.src(paths.index)
-    .pipe(gulp.dest(paths.build))
-})
-
-gulp.task('copy:scripts', function () {
-  return gulp.src('./app/dist/bundle.js')
-    .pipe(rename('bundle.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('./build/dist/'))
-})
-
-gulp.task('copy:vendor', function () {
-  return gulp.src('./app/dist/vendor.js')
-    .pipe(rename('vendor.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('./build/dist/'))
-})
-
-gulp.task('copy:css', function () {
-  return gulp.src('./app/dist/bundle.css')
-    .pipe(rename('bundle.css'))
-    .pipe(gulp.dest('./build/dist/'))
-})
-
-gulp.task('copy:assets', function () {
-  return gulp.src('./app/assets/**/*.*')
-    .pipe(gulp.dest(paths.build + 'assets/'))
-})
 
 var aws = {
   'key': sensitive.AWS_KEY,
@@ -100,19 +48,85 @@ var aws = {
   'distributionId': sensitive.CLOUDFRONT_DISTRIBUTION_ID,
   'headers': {'Cache-Control': 'max-age=315360000, no-transform, public'}
 }
+
+
 gulp.task('publish:cloudfront', function () {
-  return gulp.src('build/**/*.*')
-    .pipe(revall())
-    .pipe(gzip())
-    .pipe(s3(aws, {gzippedOnly: true}))
-    .pipe(cloudfront(aws))
+ return gulp.src('build/**/*.*')
+   .pipe(revall())
+   .pipe(gzip())
+   .pipe(s3(aws, {gzippedOnly: true}))
+   .pipe(cloudfront(aws))
 })
 
-gulp.task('clean:build', function () {
-  return gulp.src([paths.dist, paths.build], {read: false})
-        .pipe(clean())
+// copy files from app/* to build/*
+gulp.task('copy:all', ['copy:index', 'copy:scripts', 'copy:vendor', 'copy:css', 'copy:assets'])
+
+// lint and compile sources
+//gulp.task('dev', ['lint:scripts', 'vendor', 'scripts', 'styles'])
+gulp.task('dev', function(cb) {
+
+  // run in series
+  runSequence('lint:scripts', 'vendor', 'scripts', 'styles', cb)
 })
 
+gulp.task('publish', function() {
+  
+  // run in series
+  runSequence('build', 'publish:cloudfront')
+})
+
+// start the dev server, and auto-update
+gulp.task('default', ['server', 'dev', 'watch'])
+
+// build for production
+gulp.task('build', ['clean:build', 'dev', 'appcache', 'copy:all'])
+
+
+
+/*
+ * Dev compilation
+ *
+ */
+
+// init.js --> dist/bundle.js
+gulp.task('scripts', function () {
+  return browserify(paths.init)
+    .bundle({debug:true})
+    .pipe(source(outFiles.scripts))
+    .pipe(gulp.dest(paths.dist))
+})
+
+// vendor/*.js --> dist/vendor.js
+gulp.task('vendor', function () {
+  return gulp.src(paths.vendor)
+    .pipe(sourcemaps.init())
+    .pipe(concat(outFiles.vendors))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.dist))
+})
+
+// css/style.css --> dist/bundle.css
+gulp.task('styles', function () {
+  return gulp.src(paths.styles)
+    .pipe(rename(outFiles.styles))
+    .pipe(gulp.dest(paths.dist))
+})
+
+// run jshint
+gulp.task('lint:scripts', function () {
+  return gulp.src(paths.scripts)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+})
+
+
+
+/*
+ * Dev server and watcher
+ *
+ */
+
+// start the dev server
 gulp.task('server', function () {
 
   // Don't actually watch for changes, just run the server
@@ -126,16 +140,57 @@ gulp.task('watch', function () {
   gulp.watch(paths.styles, ['styles'])
 })
 
-gulp.task('copy:all', ['copy:index', 'copy:scripts', 'copy:vendor', 'copy:css', 'copy:assets'])
-gulp.task('dev', function(cb) {
-  runSequence('lint:scripts', 'vendor', 'scripts', 'styles', cb) // run in series
-                                                          // ^^ necessary to make 'dev' synchronous
+
+
+/*
+ * Production compilation
+ *
+ */
+
+// cache.appcache --> build/cache.appcache
+gulp.task('appcache', function () {
+  return gulp.src(paths.appcache)
+    .pipe(footer('\n#' + Date.now()))
+    .pipe(gulp.dest(paths.build))
 })
-gulp.task('default', ['server', 'dev', 'watch'])
-gulp.task('build', function(cb) {
-  runSequence('clean:build', 'dev', 'appcache', 'copy:all', cb) // run in series
-                                                         // ^^ necessary to make 'build' synchronous
+
+// index.html --> build/index.html
+gulp.task('copy:index', function () {
+  return gulp.src(paths.index)
+    .pipe(gulp.dest(paths.build))
 })
-gulp.task('publish', function() {
-  runSequence('build', 'publish:cloudfront') // run in series
+
+// bundle.js --> build/dist/bundle.js
+gulp.task('copy:scripts', function () {
+  return gulp.src(paths.dist + outFiles.scripts)
+    .pipe(rename(outFiles.scripts))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.buildDist))
+})
+
+// vendor.js --> build/dist/vendor.js
+gulp.task('copy:vendor', function () {
+  return gulp.src(paths.dist + outFiles.vendors)
+    .pipe(rename(outFiles.vendors))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.buildDist))
+})
+
+// bundle.css --> build/dist/bundle.css
+gulp.task('copy:css', function () {
+  return gulp.src(paths.dist + outFiles.styles)
+    .pipe(rename(outFiles.styles))
+    .pipe(gulp.dest(paths.buildDist))
+})
+
+// assets --> build/assets
+gulp.task('copy:assets', function () {
+  return gulp.src(paths.assets)
+    .pipe(gulp.dest(paths.build + 'assets/'))
+})
+
+// rm build && rm app/dist
+gulp.task('clean:build', function () {
+  return gulp.src([paths.dist, paths.build], {read: false})
+        .pipe(clean())
 })
